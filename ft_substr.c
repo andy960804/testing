7cc622dc2fe9b56da24a34ba667b0ed8060138fa
andy <andi9608@gmail.com>
2022-02-02 23:28:24 +0900

    #5

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,13 @@
 /*   By: boskim <boskim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 16:54:58 by boskim            #+#    #+#             */
-/*   Updated: 2022/02/02 23:07:44 by boskim           ###   ########seoul.kr  */
+/*   Updated: 2022/02/02 23:28:19 by boskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char	const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 	size_t	l;
