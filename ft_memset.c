5853c5e146f50ea527ef1ae8143b4c33fca28f11
andy <andi9608@gmail.com>
2022-02-02 13:11:17 +0900

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,23 +6,27 @@
 /*   By: boskim <boskim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 16:54:11 by boskim            #+#    #+#             */
-/*   Updated: 2022/02/02 09:37:36 by boskim           ###   ########seoul.kr  */
+/*   Updated: 2022/02/02 13:10:18 by boskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *p, int c, size_t len)
 {
 	size_t			i;
-	unsigned char	*b_ptr;
+	unsigned char	*p_ptr;
 
-	b_ptr = b;
+	p_ptr = p;
 	i = 0;
 	while (i < len)
 	{
-		b_ptr[i] = (unsigned char)c;
+		p_ptr[i] = (unsigned char)c;
 		i++;
 	}
-	return (b);
+	if (p_ptr == NULL)
+	{
+		return (NULL)
+	}
+	return (p);
 }
