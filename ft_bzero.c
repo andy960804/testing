ea5de0e9a441ffcaa1e09c460691f8b8508f20f8
andy <andi9608@gmail.com>
2022-02-03 09:52:28 +0900

    #8

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: boskim <boskim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 16:53:16 by boskim            #+#    #+#             */
-/*   Updated: 2022/02/03 09:49:33 by boskim           ###   ########seoul.kr  */
+/*   Updated: 2022/02/03 09:52:11 by boskim           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_bzero(void *s, size_t n)
 {
 	char	*ptr;
 	size_t	i;
-	
+
 	ptr = s;
 	i = 0;
 	while (i < n)
